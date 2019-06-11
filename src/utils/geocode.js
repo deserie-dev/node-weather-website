2f2efc5a3bfccacfc980d241b4e27f172af4012d
Deserie-dev <deserie.dev@gmail.com>
2019-06-12 01:03:02 +0200

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZGVzZXJpZS1kZXYiLCJhIjoiY2p1dG9rcXVmMDljMDQzb2cxOHY2emFwbyJ9.XI-n2Dxi3C-W-4GOP6sN5g'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
